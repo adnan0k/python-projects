df3e82225a31db22e38b44136d172b8ab464d44c
Adnan Khaliq <166370391+adnan0k@users.noreply.github.com>
2025-10-05 05:52:03 +0000

    added a login form

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world rerun debug true"
+    return render_template("index.html", name="lazzi`")
+
+@app.route('/login')
+def login():
+    return render_template('form.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
